c117f6e07ec9a1ea93c92e039ff751dbb7391375
邝永鑫 <kuangyongxin@iecas.cc>
2022-06-24 14:16:51 +0800

    feat: 测试vssue

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <ParentLayout>
+    <!-- 这里组件出不来 -->
+    <!-- <Vssue/> -->
+  </ParentLayout>
+</template>
+
+<script setup lang="ts">
+import { onMounted, computed } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { usePageData } from "@vuepress/client";
+
+import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+import 'vssue/dist/vssue.css';
+
+
+</script>
+
+<style lang="scss">
+</style>
